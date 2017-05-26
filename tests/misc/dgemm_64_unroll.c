665b2353f754a0820bdf918f48d0991a4f699a8e
Tetsuya ODAJIMA <tetsuya.odajima@riken.jp>
2017-05-26 17:14:25 +0900

    Add test of dgemm_64_unroll

---
+++ tests/misc/dgemm_64_unroll.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+double get_dtime(void) {
+  struct timespec ts;
+  clock_gettime(CLOCK_MONOTONIC, &ts);
+  return ((double)(ts.tv_sec) + (double)(ts.tv_nsec) * 1e-9);
+}
+
+#define N 64
+
+double A[N][N], B[N][N], C[N][N];
+
+int main(int argc, char **argv)
+{
+  int i, j, k;
+  static double time;
+ 
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < N; j++) {
+      A[i][j] = (double)(i * N + j);
+      if (i == j) {
+      	B[i][j] = 1.0;
+      } else {
+      	B[i][j] = 0.0;
+      }
+      C[i][j] = 0.0;
+    }
+  }
+
+    time = get_dtime();
+
+  for (i = 0; i < N; i++) {
+    for (k = 0; k < N; k++) {
+#pragma clang loop unroll_count(2)
+      for (j = 0; j < N; j++) {
+  	C[i][j] += A[i][k] * B[k][j];
+      }
+    }
+  }
+
+  time = get_dtime() - time;
+
+#if 1
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < N; j++) {
+      if ( (fabs(C[i][j] - A[i][j]) > 1e-15) ) {
+      	printf("%lf, actual : %lf\n", A[i][j], C[i][j]);
+      	printf("FAILED\n");
+      	exit(-1);
+      }
+    }
+  }
+  printf("PASS\n");
+#else
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < N; j++) {
+      printf("%.1lf\t", C[i][j]);
+    }
+    printf("\n");
+  }
+#endif
+
+  printf("time = %lf [msec]\n", time * 1000.0);
+
+  return 0;
+}
