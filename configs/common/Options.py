c41442d2ffd387ea836ba4d84292462e31333ac3
Takanori Watanabe <takawata@init-main.com>
2018-05-25 18:44:53 +0900

    Set default cache-line length to 256
    Change-Id: I4d176df8e91014c929cc9f327b24a96f8293d650

--- configs/common/Options.py
+++ configs/common/Options.py
@@ -110,7 +110,7 @@ def addNoISAOptions(parser):
     parser.add_option("--l1i_assoc", type="int", default=2)
     parser.add_option("--l2_assoc", type="int", default=8)
     parser.add_option("--l3_assoc", type="int", default=16)
-    parser.add_option("--cacheline_size", type="int", default=64)
+    parser.add_option("--cacheline_size", type="int", default=256)
 
     # Enable Ruby
     parser.add_option("--ruby", action="store_true")
