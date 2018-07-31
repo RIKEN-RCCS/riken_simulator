8241f450b65f5eab8fb286b810b539fcbeee1497
Takanori Watanabe <takawata@init-main.com>
2018-07-31 18:06:29 +0900

    Set Process stack size.
    Change-Id: Id19bb0f343ae524c1c31f963881a7adf01bcad54

--- src/sim/Process.py
+++ src/sim/Process.py
@@ -45,7 +45,8 @@ class Process(SimObject):
     useArchPT = Param.Bool('false', 'maintain an in-memory version of the page\
                             table in an architecture-specific format')
     kvmInSE = Param.Bool('false', 'initialize the process for KvmCPU in SE')
-    maxStackSize = Param.MemorySize('64MB', 'maximum size of the stack')
+    #maxStackSize = Param.MemorySize('64MB', 'maximum size of the stack')
+    maxStackSize = Param.MemorySize('256MB', 'maximum size of the stack')
 
     uid = Param.Int(100, 'user id')
     euid = Param.Int(100, 'effective user id')
