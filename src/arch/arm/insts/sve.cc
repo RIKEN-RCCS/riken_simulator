5cbf1e1dd8fb1cae1caaa491be149609bd318a96
Javier Setoain <javier.setoain@arm.com>
2018-09-07 16:08:39 +0100

    arch-arm: Fix SVE CMP<cc> instructions.
    Change-Id: Ifa471c0b6fdbad658e5d697f1bf5eedea4897edf
    Signed-off-by: Giacomo Gabrielli <giacomo.gabrielli@arm.com>

--- src/arch/arm/insts/sve.cc
+++ src/arch/arm/insts/sve.cc
@@ -451,9 +451,9 @@ SveIntCmpOp::generateDisassembly(Addr pc,
     std::stringstream ss;
     printMnemonic(ss, "", false);
     printPredReg(ss, dest);
-    ccprintf(ss, "/z, ");
-    printPredReg(ss, gp);
     ccprintf(ss, ", ");
+    printPredReg(ss, gp);
+    ccprintf(ss, "/z, ");
     printVecReg(ss, op1, true);
     ccprintf(ss, ", ");
     if (op2IsWide) {
