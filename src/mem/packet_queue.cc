e9d734b9e26e4b410bb7c4813fb52086fae2b6db
Takanori Watanabe <takawata@init-main.com>
2018-10-01 16:42:25 +0900

    Correct panic message.
    Change-Id: I4921a38faf6e47338e14c43f8a6e3ff0a1c8510e

--- src/mem/packet_queue.cc
+++ src/mem/packet_queue.cc
@@ -119,7 +119,7 @@ PacketQueue::schedSendTiming(PacketPtr pkt, Tick when, bool force_order)
     // add a very basic sanity check on the port to ensure the
     // invisible buffer is not growing beyond reasonable limits
     if (!_disableSanityCheck && transmitList.size() > 1000) {
-        panic("Packet queue %s has grown beyond 100 packets\n",
+        panic("Packet queue %s has grown beyond 1000 packets\n",
               name());
     }
 
